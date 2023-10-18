7a939eca4cce7ebe6ed3b62bd9512917130b51d9
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-18 11:47:23 -0300

    components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,33 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 
-import { Header } from "../../components/Button/Header"
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
   return (
     <Container>
       <Header/>
+
+      <ButtonText title="Excluir nota"/>
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
       
       <Button title="Voltar"/>
     </Container>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.div`
 width: 100%;
@@ -9,5 +9,16 @@ grid-template-rows: 105px auto;
 grid-template-areas:
 "header"
 "content"
-;
+`;
+
+export const Links = styled.ul`
+list-style: none;
+
+> li {
+    margin-top: 12px;
+
+    a {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+}
 `;
