9bc19bf3c0182e8e589acd1ef9c92d0c839307ac
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-19 11:35:50 -0300

    Create note button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from '../../components/Header';
@@ -23,7 +24,10 @@ export function Home() {
 
             <Content></Content>
 
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus />
+                Criar nota
+            </NewNote>
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -57,4 +57,15 @@ grid-area: content;
 
 export const NewNote = styled.button`
 grid-area: newnote;
+
+background-color: ${({ theme }) => theme.COLORS.ORANGE};
+border: none;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+svg {
+    margin-right: 8px;
+}
 `;
