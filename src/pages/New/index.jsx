d2154d0dd98513fcd3bcad5b89b7b4551f56b5b4
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-23 23:02:25 -0300

    styled component NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,9 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+
 
 import { Container, Form } from './styles'
 
@@ -17,8 +20,12 @@ export function New() {
           </header>
 
           <Input placeholder="Título" />
-
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis"> 
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
         </Form>
       </main>
     </Container>
